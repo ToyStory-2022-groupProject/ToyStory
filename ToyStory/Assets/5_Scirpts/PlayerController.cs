007142a805e87103b15e58ce8b35c2350c298ce5
eomjimin <jimini0920@gmail.com>
2022-04-07 01:39:22 +0900

    애니메이션

--- ToyStory/Assets/5_Scirpts/PlayerController.cs
+++ ToyStory/Assets/5_Scirpts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed = 0.1f;
     [SerializeField] float walkSpeed = 0;
     [SerializeField] float runSpeed = 1.0f;
+    [SerializeField] float jumpPower = 3.0f;
     private CapsuleCollider col;
     private Rigidbody rb;
     private Animator anim;
@@ -26,21 +27,48 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         currentBaseState = anim.GetCurrentAnimatorStateInfo(0);
-        if(Input.GetKey(KeySetting.keys[KeyAction.LEFT])||Input.GetKey(KeySetting.keys[KeyAction.RIGHT]))
+        if(Input.GetKey(KeySetting.keys[KeyAction.LEFT]))
         {
+              if(Input.GetKey(KeySetting.keys[KeyAction.WALK]))
+            {
+                anim.SetFloat("Speed", walkSpeed);
+                transform.Translate(new Vector3(0,0,speed*0.5f*Time.deltaTime));
+            }
+            else
+            {
+                anim.SetFloat("Speed", runSpeed);
+                transform.Translate(new Vector3(0,0,speed*Time.deltaTime));
+            }
+            transform.rotation = Quaternion.Euler(0,180,0);
             anim.SetBool("Move", true);
-            transform.Translate(new Vector3(0,0,speed*Time.deltaTime));
         }
+        else if(Input.GetKey(KeySetting.keys[KeyAction.RIGHT]))
+        {
+            if(Input.GetKey(KeySetting.keys[KeyAction.WALK]))
+            {
+                anim.SetFloat("Speed", walkSpeed);
+                transform.Translate(new Vector3(0,0,speed*0.5f*Time.deltaTime));
+            }
+            else
+            {
+                anim.SetFloat("Speed", runSpeed);
+                transform.Translate(new Vector3(0,0,speed*Time.deltaTime));
+            }
+            transform.rotation = Quaternion.Euler(0,0,0);
+            anim.SetBool("Move", true);  
+        }
+        else if(Input.GetKeyUp(KeySetting.keys[KeyAction.LEFT])||Input.GetKeyUp(KeySetting.keys[KeyAction.RIGHT]))
+            anim.SetBool("Move", false);
 
-        if (Input.GetButtonDown("Jump")) // 점프키를 누르면
+
+        if (Input.GetKeyDown(KeySetting.keys[KeyAction.JUMP])) // 점프키를 누르면
         {
             if (!anim.IsInTransition(0)) // 현재 트랜지션이 수행 중이지 않다면
             {
-                rb.AddForce(Vector3.up, ForceMode.VelocityChange);
+                rb.AddForce(Vector3.up * jumpPower);
                 anim.SetBool("Jump", true); // 점프
             }
-        }
-        
+        } 
         if (currentBaseState.fullPathHash == jumpState && !anim.IsInTransition(0)) // 점프 중인 경우
         {
             anim.SetBool("Jump", false); // 이미 점프를 수행 중이므로 이제 FALSE로 설정
